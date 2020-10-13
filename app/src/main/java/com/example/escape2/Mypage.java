2f3cf3c7b3a5035dcf85b8a23ad18f32a67f8b2f
MYCOM <then0925@gmail.com>
2020-10-13 15:33:22 +0900

    두번째 버전

--- app/src/main/java/com/example/escape2/Mypage.java
+++ app/src/main/java/com/example/escape2/Mypage.java
@@ -14,15 +14,14 @@ import android.widget.Toast;
 public class Mypage extends AppCompatActivity {
     private final int GET_GALLERY_IMAGE = 200;
     private ImageView imageview;
-    EditText editText;
-    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
 
-        Button button=(Button) findViewById(R.id.button5);
+        Button button = (Button) findViewById(R.id.button5);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,38 +29,7 @@ public class Mypage extends AppCompatActivity {
             }
         });
 
-        editText=findViewById(R.id.textView);
-        textView=findViewById(R.id.textView);
 
-        editText=findViewById(R.id.textView2);
-        textView=findViewById(R.id.textView2);
-
-        editText=findViewById(R.id.textView3);
-        textView=findViewById(R.id.textView3);
-
-        editText=findViewById(R.id.textView7);
-        textView=findViewById(R.id.textView7);
-    }// onCreate()..
-
-    public void clickSetBt(View view) {    // Set버튼 클릭 시    SharedPreferences에 값 저장.
-        if(editText.getText().toString().isEmpty()){ // 공백 또는 size=0이면
-            Toast.makeText(this, "값을 입력해주세요.", Toast.LENGTH_SHORT).show();
-        }
-        else {
-            SharedPreferences sharedPreferences= getSharedPreferences("test", MODE_PRIVATE);    // test 이름의 기본모드 설정
-            SharedPreferences.Editor editor= sharedPreferences.edit(); //sharedPreferences를 제어할 editor를 선언
-            editor.putString("inputText",editText.getText().toString()); // key,value 형식으로 저장
-            editor.commit();    //최종 커밋. 커밋을 해야 저장이 된다.
-            Toast.makeText(this, "저장되었습니다.", Toast.LENGTH_SHORT).show();
-
-        }
-    }// clickSetBt()..
-
-    public void clickGetBt(View view) {     // Get버튼 클릭 시   SharedPreferences에 값 불러오기.
-        SharedPreferences sharedPreferences= getSharedPreferences("test", MODE_PRIVATE);    // test 이름의 기본모드 설정, 만약 test key값이 있다면 해당 값을 불러옴.
-        String inputText = sharedPreferences.getString("inputText","");
-        textView.setText(inputText);    // TextView에 SharedPreferences에 저장되어있던 값 찍기.
-        Toast.makeText(this, "불러오기 하였습니다..", Toast.LENGTH_SHORT).show();
-    }// clickGetBt()..
-}// MainActivity class..
+    }
+}
 
--- app/src/main/java/com/example/escape2/registerpage.java
+++ app/src/main/java/com/example/escape2/registerpage.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class registerpage extends AppCompatActivity {
@@ -31,5 +32,8 @@ public class registerpage extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
+
+
+
+}
 }
